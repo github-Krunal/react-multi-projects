fd5349dd5962bfe0c7e31a018c22d413ac4f0df8
githubkrunal <githubkrunal@gmail.com>
2024-01-01 12:27:02 +0530

    admin page side bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import AdminMain from './ecommerce/component/admincomponent/adminmain';
+import Page1 from './ecommerce/component/admincomponent/page1';
+import Page2 from './ecommerce/component/admincomponent/page2';
 import Cart from './ecommerce/component/cart';
 import Home from './ecommerce/component/home';
 import ProductCart from './ecommerce/component/ProductCart';
@@ -16,6 +19,10 @@ function App() {
           <Route element={<Topbar />}>
             <Route path="/" element={<Home />} />
             <Route path="/cart" element={<Cart />} />
+            <Route path="/admin" element={<AdminMain />} >
+              <Route path='page1' element={<Page1/>}></Route>
+              <Route path='page2' element={<Page2/>}></Route>
+            </Route>
             <Route path="/search" element={<SearchProduct />} />
             <Route path="/product-details/:productID" element={<ProductDetails />} />
           </Route>
---
+++ src/ecommerce/component/admincomponent/page1.tsx
@@ -0,0 +1,7 @@
+const Page1=()=>{
+    return(
+        <div>page1</div>
+    )
+}
+
+export default Page1;
