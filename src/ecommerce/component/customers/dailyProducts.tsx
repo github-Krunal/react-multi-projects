e2c34186c2f1ebeafb1e671ba8b662424572ee4c
githubkrunal <githubkrunal@gmail.com>
2024-01-10 23:38:49 +0530

    product card

--- src/ecommerce/component/customers/dailyProducts.tsx
+++ src/ecommerce/component/customers/dailyProducts.tsx
@@ -1,21 +1,28 @@
 import ProductCard from "../global/productCard";
 
-const DailyProducts=()=>{
-    return(
-        <div>
-            <div style={{marginTop:"50px"}}>
-                <div style={{display:'flex',justifyContent:"space-between",alignItems:"center",marginBottom:'25px'}}>
-                    <h1 style={{fontWeight:"bold"}}>All fresh products daily!</h1>
-                    <div>
-                        <button className="categories-button">All</button>
-                        <button className="categories-button">Vegetables</button>
-                    </div>
-                </div>
-
-<ProductCard/>
-            </div>
+const DailyProducts = () => {
+  return (
+    <div>
+      <div style={{ marginTop: "50px" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: "25px",
+          }}
+        >
+          <h1 style={{ fontWeight: "bold" }}>All fresh products daily!</h1>
+          <div>
+            <button className="categories-button">All</button>
+            <button className="categories-button">Vegetables</button>
+          </div>
         </div>
-    )
-}
+
+        <ProductCard />
+      </div>
+    </div>
+  );
+};
 
 export default DailyProducts;
--- src/ecommerce/component/global/productCard.tsx
+++ src/ecommerce/component/global/productCard.tsx
@@ -1,8 +1,53 @@
+import { FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material"
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 const ProductCard=()=>{
     return (
         <div>
-            ProductCard
-        </div>
+            <div style={{width:'300px',boxShadow:'0 0 10px #a9a3a3'}}>
+            <div style={{height:"300px"}}>
+                <img src="https://hongotheme.myshopify.com/cdn/shop/products/grosery-product-01.jpg?v=1692168983&width=360" alt="" style={{width:'100%',height:'100%',objectFit:'cover'}} />
+            </div>
+        <div style={{padding:'0px 20px 25px 30px '}}>
+            <h4 style={{color:"#aa9e9e"}}>Fresh fruits</h4>
+            <h2>Fresh watermelon</h2>
+            <div style={{marginTop:'15px',display:"flex",gap:'10px'}}>
+            <FormControl sx={{  minWidth: 120 }} size="small">
+      <InputLabel id="demo-select-small-label">Age</InputLabel>
+      <Select
+        labelId="demo-select-small-label"
+        id="demo-select-small"
+        label="Age"
+       
+      >
+        <MenuItem value="">
+          <em>None</em>
+        </MenuItem>
+        <MenuItem value={10}>Ten</MenuItem>
+        <MenuItem value={20}>Twenty</MenuItem>
+        <MenuItem value={30}>Thirty</MenuItem>
+      </Select>
+    </FormControl>
+
+    <TextField
+          id="outlined-number"
+          label="Number"
+          type="number"
+          size="small"
+        />
+            </div>
+
+            <div style={{marginTop:'15px',display:"flex",gap:'10px',justifyContent:'space-between',alignItems:"center"}}>
+                <div>
+                    <div style={{fontSize:"20px",color:'#429519'}}>RS. 300</div>
+                    <div style={{fontSize:"16px",color:'#828282',textDecoration:'line-through'}}>RS. 500</div>
+                </div>
+                <div>
+                    <button className="cart-btn">
+                       <ShoppingBagIcon/> <span style={{alignSelf:'center',height:'12px',marginLeft:'5px'}}>ADD</span>
+                        </button>
+                </div>
+                </div>
+        </div></div></div>
     )
 }
 
